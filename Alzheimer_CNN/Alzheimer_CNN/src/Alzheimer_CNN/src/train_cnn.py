60123a1724959276f30de30caae1182db9caf183
fealiamofili-lab <fealiam.ofili@csuglobal.edu>
2026-03-04 11:04:42 -0500

    Add placeholder CNN training script

---
+++ Alzheimer_CNN/Alzheimer_CNN/src/Alzheimer_CNN/src/train_cnn.py
@@ -0,0 +1,10 @@
+"""
+CNN Training Skeleton for MRI Classification
+"""
+
+def train_model(X_train, y_train):
+    """
+    Placeholder for CNN training
+    """
+    print("Training CNN model... (placeholder)")
+    return None
