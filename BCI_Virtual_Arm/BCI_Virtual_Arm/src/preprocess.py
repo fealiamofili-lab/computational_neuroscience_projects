401a68da40c93cc5a355f6e417e79fbf4b856890
fealiamofili-lab <fealiam.ofili@csuglobal.edu>
2026-03-04 10:57:38 -0500

    Add placeholder EEG preprocessing script

---
+++ BCI_Virtual_Arm/BCI_Virtual_Arm/src/preprocess.py
@@ -0,0 +1,21 @@
+"""
+EEG Preprocessing Module
+Author: Fealiam Ofili
+Status: In Progress
+"""
+
+import numpy as np
+
+def bandpass_filter(eeg_signal, low_freq=1, high_freq=40, fs=250):
+    """
+    Placeholder function for bandpass filtering EEG signals.
+    """
+    # TODO: Implement actual filtering
+    return eeg_signal
+
+def remove_artifacts(eeg_signal):
+    """
+    Placeholder function to remove artifacts from EEG signals.
+    """
+    # TODO: Implement artifact removal
+    return eeg_signal
