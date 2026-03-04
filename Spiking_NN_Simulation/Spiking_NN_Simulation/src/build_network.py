0a6e1fd7ffda8fff7aecd4c9b968ec236101f4bc
fealiamofili-lab <fealiam.ofili@csuglobal.edu>
2026-03-04 11:02:29 -0500

    Add placeholder network builder script

---
+++ Spiking_NN_Simulation/Spiking_NN_Simulation/src/build_network.py
@@ -0,0 +1,10 @@
+"""
+Spiking Neural Network Builder (Brian2)
+"""
+
+def build_small_network():
+    """
+    Build a small excitatory/inhibitory network (placeholder)
+    """
+    print("Building network... (placeholder)")
+    return None
