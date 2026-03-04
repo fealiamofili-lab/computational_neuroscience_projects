44507a327862d4d824cb2323da6fc2328cf78b44
fealiamofili-lab <fealiam.ofili@csuglobal.edu>
2026-03-04 10:59:26 -0500

    Add placeholder CNN training script

---
+++ BCI_Virtual_Arm/BCI_Virtual_Arm/src/BCI_Virtual_Arm/src/BCI_Virtual_Arm/src/train_model.py
@@ -0,0 +1,10 @@
+"""
+CNN Training Skeleton for BCI
+"""
+
+def train_cnn(features, labels):
+    """
+    Placeholder for CNN training.
+    """
+    print("Training CNN model... (placeholder)")
+    return None
