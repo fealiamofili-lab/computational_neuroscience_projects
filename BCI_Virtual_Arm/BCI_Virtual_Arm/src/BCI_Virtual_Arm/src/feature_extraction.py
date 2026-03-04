b0c4da1c80f88f26365ccf2ac967096bae40ab5c
fealiamofili-lab <fealiam.ofili@csuglobal.edu>
2026-03-04 10:58:39 -0500

    Add placeholder EEG feature extraction script

---
+++ BCI_Virtual_Arm/BCI_Virtual_Arm/src/BCI_Virtual_Arm/src/feature_extraction.py
@@ -0,0 +1,12 @@
+"""
+EEG Feature Extraction
+"""
+
+import numpy as np
+
+def extract_psd(eeg_signal):
+    """
+    Placeholder for power spectral density feature extraction.
+    """
+    # TODO: Implement PSD computation
+    return np.random.rand(10)  # dummy feature vector
