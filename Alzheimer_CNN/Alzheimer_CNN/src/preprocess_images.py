4e47a49242ff29b8471a546731ed76e166c549f3
fealiamofili-lab <fealiam.ofili@csuglobal.edu>
2026-03-04 11:04:19 -0500

    Add placeholder MRI preprocessing script

---
+++ Alzheimer_CNN/Alzheimer_CNN/src/preprocess_images.py
@@ -0,0 +1,10 @@
+"""
+MRI Image Preprocessing
+"""
+
+def resize_images(images, target_size=(128, 128)):
+    """
+    Placeholder resizing function
+    """
+    print("Resizing images... (placeholder)")
+    return images
